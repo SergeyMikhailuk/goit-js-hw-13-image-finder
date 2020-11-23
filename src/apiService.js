2493d6b20dfe954e85759218924dfa6ba3252ee3
Mikhailyuk Sergey <vzxyqw@gmail.com>
2020-11-23 21:23:36 +0200

    done work without additions

--- src/apiService.js
+++ src/apiService.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 async function getImages(params) {
-  const { query, page, key } = params;
+  const { query, page, key, perpage } = params;
   try {
     const response = await axios.get(
-      `?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=12&key=${key}`,
+      `?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=${perpage}&key=${key}`,
     );
 
     return response;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import './styles.css';
-import getImages from './fetchImages';
+import getImages from './apiService';
 import imagesTemplateHbs from './templates/imagesDesk.hbs';
 
 const formRef = document.querySelector('.search-form');
 const inputRef = document.querySelector('.search-form input');
 const galleryRef = document.querySelector('.gallery');
+const searchBtn = document.querySelector('.search-btn');
+const clearBtnRef = document.querySelector('.clear-btn');
+const loadMoreBtnRef = document.querySelector('.load-more-btn');
+const loaderRef = document.querySelector('.lds-roller');
 
 formRef.addEventListener('submit', onSubmit);
+searchBtn.addEventListener('click', onSubmit);
+loadMoreBtnRef.addEventListener('click', onSubmit);
+clearBtnRef.addEventListener('click', handleCleanup);
+
+let queryString = '';
+function handleCleanup() {
+  options.page = 1;
+  galleryRef.innerHTML = '';
+  inputRef.value = '';
+  loadMoreBtnRef.classList.add('is-hidden');
+}
 
 const options = {
   page: 1,
@@ -15,18 +30,38 @@ const options = {
 
 function onSubmit(event) {
   event.preventDefault();
+  loaderRef.classList.add('show');
+  const amountRow = Math.floor(window.innerWidth / 354) * 3;
+  console.log(amountRow);
+  if (queryString !== inputRef.value) {
+    options.page = 1;
+    queryString = inputRef.value;
+  }
   const searchOptions = {
     ...options,
     query: inputRef.value,
+    perpage: amountRow,
   };
-  getImages(searchOptions).then(handleResponse);
   options.page += 1;
+  getImages(searchOptions).then(handleResponse).then(showLoadMoreBtn);
+}
+
+function showLoadMoreBtn() {
+  loaderRef.classList.remove('show');
+  if (options.page !== 1) {
+    loadMoreBtnRef.classList.remove('is-hidden');
+  }
+  console.log(`inner Height : ${innerHeight}`);
+  console.log(`inner Width : ${innerWidth}`);
+  scroll({
+    top: 99999999,
+    behavior: 'smooth',
+  });
 }
 
 function handleResponse(response) {
   const images = response.data.hits;
   showImages(images);
-  console.log(images);
 }
 
 function showImages(images) {
